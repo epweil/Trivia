3ba6bc626efaa86bc8ab7e306f26b92afdcbb5bf
Ethan Weilheimer <ethanweilheimer@Ethans-MacBook-Pro-2.local>
2022-10-14 19:06:29 -0500

    Hello World

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React, { MutableRefObject, useContext, useEffect, useRef } from 'react';
+import { GestureResponderEvent, Modal, Text, View } from "react-native";
+
+export default function App() {
+
+    return (
+        <Text>Hello</Text>
+    )
+
+}
+
+
